3237c074c8b2a473033e86bec7f2d88c1759e23f
user <user@edi20>
2017-04-14 14:15:43 +0900

    이대희 바보 바보 바보 추가

--- 20120/src/ex1.java
+++ 20120/src/ex1.java
@@ -2,7 +2,8 @@
 public class ex1 {
 
 	public static void main(String[] args) {
-		System.out.println("이대희 멍청구리구리 아연");
+		System.out.println("이대희 멍청구리 구리 아연");
+		System.out.println("이대희 바보 바보 바보");
 
 	}
 
