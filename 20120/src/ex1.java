54c6da6d6e5e4044fa51f4ba4193e46da0d89f94
user <user@edi20>
2017-04-14 14:18:41 +0900

    이대희 이해못하네

--- 20120/src/ex1.java
+++ 20120/src/ex1.java
@@ -4,6 +4,7 @@ public class ex1 {
 	public static void main(String[] args) {
 		System.out.println("이대희 멍청구리 구리 아연");
 		System.out.println("이대희 바보 바보 바보");
+		System.out.println("이대희 이해 못하네");
 
 	}
 
